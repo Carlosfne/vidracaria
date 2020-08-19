acfb80dc64edd89f19b3423eaa575b16e566f651
lvreis <luizvitorreis@gmail.com>
2020-08-18 21:29:49 -0400

    env

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 // Conexão com a url da API
 const api_veiculos = axios.create({
 
-    // baseURL: process.env.REACT_APP_API_URL
-    baseURL: 'https://vidracariacfm.herokuapp.com/'
+    baseURL: process.env.REACT_APP_API_URL
+    // baseURL: 'https://vidracariacfm.herokuapp.com/'
 
 })
 
